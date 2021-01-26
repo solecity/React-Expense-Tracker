2a0638c6085c782ad5ba16dcd49adf4ee67600b5
Mariana Moreira <marianaccmoreira@gmail.com>
2021-01-26 22:09:12 +0000

    income tracker exercise

---
+++ src/components/IncomeList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import IncomeItem from "./IncomeItem";
+
+function IncomeList({ income, setIncome }) {
+  const removeIncome = (i) => {
+    let temp = income.filter((v, index) => index !== i);
+
+    setIncome(temp);
+  };
+
+  const sortByDate = (a, b) => {
+    return a.date - b.date;
+  };
+
+  return (
+    <div className="income-list">
+      {income.sort(sortByDate).map((value, index) => (
+        <IncomeItem
+          key={index}
+          index={index}
+          income={value}
+          removeIncome={removeIncome}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default IncomeList;
